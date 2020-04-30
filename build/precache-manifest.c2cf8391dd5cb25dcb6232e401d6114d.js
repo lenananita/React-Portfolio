ba05ef0e781cfc6730624ed5c662d2aa5d0082ce
Elenita Oliveira <nana.elenita222@gmail.com>
2020-04-29 23:37:59 -0300

    build

---
+++ build/precache-manifest.c2cf8391dd5cb25dcb6232e401d6114d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee47a91090e3116e3db9",
+    "url": "/static/css/main.8f927048.chunk.css"
+  },
+  {
+    "revision": "ee47a91090e3116e3db9",
+    "url": "/static/js/main.ee47a910.chunk.js"
+  },
+  {
+    "revision": "63fb5b41de279ed09cdd",
+    "url": "/static/js/1.63fb5b41.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ab4493b147c4263a22ba508e9e1c2330",
+    "url": "/index.html"
+  }
+];
